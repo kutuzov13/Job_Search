2e04e68cddd4d0586f54d0ef6479973a7b0b9b1c
kutuzov_em <kutuzov-1313@yandex.ru>
2021-03-17 16:58:51 +0300

    add func search_by_salary():

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 import requests
 
 
-def search_vacancies_programmer():
+def search_vacancies_programmer(program_lang):
     headers = {'User-Agent': 'HH-User-Agent'}
-    params = {'text': 'NAME:Программист',
+    params = {'text': f'Программист {program_lang}',
               'area': 1}
-    response = requests.get('https://api.hh.ru/vacancies', params=params, headers=headers).json()['items']
-    return response
+    response = requests.get('https://api.hh.ru/vacancies', params=params, headers=headers)
+    return response.json()['found']
+
+
+def search_by_salary():
+    headers = {'User-Agent': 'HH-User-Agent'}
+    params = {'text': 'Программист Python',
+              'area': 1,
+              'only_with_salary': 'true'}
+    response = requests.get('https://api.hh.ru/vacancies/', params=params, headers=headers)
+    for vacancy in response.json()['items']:
+        print(vacancy['salary'])
+
+
+programmer_languages = {'JavaScript': search_vacancies_programmer('JavaScript'),
+                        'Java': search_vacancies_programmer('Java'),
+                        'Python': search_vacancies_programmer('Python'),
+                        'Ruby:': search_vacancies_programmer('Ruby'),
+                        'PHP': search_vacancies_programmer('PHP'),
+                        'C++': search_vacancies_programmer('C++'),
+                        'C#': search_vacancies_programmer('C#'),
+                        'C': search_vacancies_programmer('C'),
+                        'Go': search_vacancies_programmer('Go'),
+                        'Shell': search_vacancies_programmer('Shell')}
+
+
+search_by_salary()
