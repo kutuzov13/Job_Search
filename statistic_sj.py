a6fa0312892e5fab1f9d17c7bec319cbf1cd1f32
kutuzov_em <kutuzov-1313@yandex.ru>
2021-03-19 18:59:38 +0300

    add func predict_salary()

--- statistic_sj.py
+++ statistic_sj.py
@@ -8,32 +8,29 @@ load_dotenv()
 TOKEN_SUPER_JOB = os.getenv('TOKEN_SUPER_JOB')
 
 
-def vacancy_name(token):
+def predict_rub_salary_for_sj(token, program_language):
     api_super_job = 'https://api.superjob.ru/2.0/vacancies/'
     headers = {'X-Api-App-Id': token}
     params = {'town': 'Москва',
-              'keyword': 'Программист'}
+              'keyword': f'Программист {program_language}'}
 
     response = requests.get(api_super_job, params=params, headers=headers)
     response.raise_for_status()
 
-    vacancies_programmers = response.json()['objects']
-    for data in vacancies_programmers:
-        print(f'{data["profession"]}, {data["town"]["title"]}, от: {data["payment_from"]} до {data["payment_to"]}')
+    yield from response.json()['objects']
 
 
-def predict_rub_salary_for_sj(token):
-    api_super_job = 'https://api.superjob.ru/2.0/vacancies/'
-    headers = {'X-Api-App-Id': token}
-    params = {'town': 'Москва',
-              'keyword': 'Программист'}
-
-    response = requests.get(api_super_job, params=params, headers=headers)
-    response.raise_for_status()
-
-    vacancies_programmers = response.json()['objects']
-    for data in vacancies_programmers:
-        print(f'{data["profession"]}, {data["town"]["title"]}, от: {data["payment_from"]} до {data["payment_to"]}')
+def predict_salary(salary_from, salary_to):
+    if salary_from != 0 and salary_to != 0:
+        return int(salary_from + salary_to / 2)
+    elif salary_from == 0 and salary_to != 0:
+        return salary_to
+    elif salary_from != 0 and salary_to == 0:
+        return salary_from
+    else:
+        pass
 
 
-predict_rub_salary_for_sj(TOKEN_SUPER_JOB)
+if __name__ == '__main__':
+    for i in predict_rub_salary_for_sj(TOKEN_SUPER_JOB, 'Python'):
+        print(predict_salary(i['payment_from'], i['payment_to']))
