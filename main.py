dc7042a6f28f6de28670f71f0fa0cf4d53a20b7b
kutuzov_em <kutuzov-1313@yandex.ru>
2021-03-19 13:31:27 +0300

    Update dict, add refactoring func predict_rub_salary(program_lang)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import statistics
+
 import requests
 
 
@@ -9,36 +11,56 @@ def search_vacancies_programmer(program_lang):
     return response.json()['found']
 
 
-def predict_salary(name_vacancy):
+def predict_rub_salary(program_lang):
     headers = {'User-Agent': 'HH-User-Agent'}
-    params = {'text': name_vacancy,
+    params = {'text': f'Программист {program_lang}',
               'area': 1,
               'only_with_salary': 'true'}
     response = requests.get(f'https://api.hh.ru/vacancies/', params=params, headers=headers)
     salary_from_to = response.json()['items']
-    return salary_from_to
+
+    expected_salary = []
+    for vacancy in salary_from_to:
+        if vacancy['salary']['from'] and vacancy['salary']['to'] and vacancy['salary']['currency'] == 'RUR':
+            expected_salary.append((vacancy['salary']['from'] + vacancy['salary']['to']) / 2)
+            if vacancy['salary']['from'] and vacancy['salary']['to'] is None:
+                expected_salary.append(vacancy['salary']['from'] * 1.2)
+                if vacancy['salary']['to'] and vacancy['salary']['from'] is None:
+                    expected_salary.append(vacancy['salary']['to'] * 0.8)
+    return expected_salary
 
 
-programmer_languages = {'JavaScript': search_vacancies_programmer('JavaScript'),
-                        'Java': search_vacancies_programmer('Java'),
-                        'Python': search_vacancies_programmer('Python'),
-                        'Ruby:': search_vacancies_programmer('Ruby'),
-                        'PHP': search_vacancies_programmer('PHP'),
-                        'C++': search_vacancies_programmer('C++'),
-                        'C#': search_vacancies_programmer('C#'),
-                        'C': search_vacancies_programmer('C'),
-                        'Go': search_vacancies_programmer('Go'),
-                        'Shell': search_vacancies_programmer('Shell')}
+programmer_languages = {'JavaScript': {'vacancies_found': search_vacancies_programmer('JavaScript'),
+                                       'vacancies_processed': len(predict_rub_salary('JavaScript')),
+                                       'average_salary': int(statistics.mean(predict_rub_salary('JavaScript')))},
+                        'Java': {'vacancies_found': search_vacancies_programmer('Java'),
+                                 'vacancies_processed': len(predict_rub_salary('Java')),
+                                 'average_salary': int(statistics.mean(predict_rub_salary('Java')))},
+                        'Python': {'vacancies_found': search_vacancies_programmer('Python'),
+                                   'vacancies_processed': len(predict_rub_salary('Python')),
+                                   'average_salary': int(statistics.mean(predict_rub_salary('Python')))},
+                        'Ruby:': {'vacancies_found': search_vacancies_programmer('Ruby'),
+                                  'vacancies_processed': len(predict_rub_salary('Ruby')),
+                                  'average_salary': int(statistics.mean(predict_rub_salary('Ruby')))},
+                        'PHP': {'vacancies_found': search_vacancies_programmer('PHP'),
+                                'vacancies_processed': len(predict_rub_salary('PHP')),
+                                'average_salary': int(statistics.mean(predict_rub_salary('PHP')))},
+                        'C++': {'vacancies_found': search_vacancies_programmer('C++'),
+                                'vacancies_processed': len(predict_rub_salary('C++')),
+                                'average_salary': int(statistics.mean(predict_rub_salary('C++')))},
+                        'C#': {'vacancies_found': search_vacancies_programmer('C#'),
+                               'vacancies_processed': len(predict_rub_salary('C#')),
+                               'average_salary': int(statistics.mean(predict_rub_salary('C#')))},
+                        'C': {'vacancies_found': search_vacancies_programmer('C'),
+                              'vacancies_processed': len(predict_rub_salary('C')),
+                              'average_salary': int(statistics.mean(predict_rub_salary('C')))},
+                        'Go': {'vacancies_found': search_vacancies_programmer('Go'),
+                               'vacancies_processed': len(predict_rub_salary('Go')),
+                               'average_salary': int(statistics.mean(predict_rub_salary('Go')))},
+                        'Shell': {'vacancies_found': search_vacancies_programmer('Shell'),
+                                  'vacancies_processed': len(predict_rub_salary('Shell')),
+                                  'average_salary': int(statistics.mean(predict_rub_salary('Shell')))}}
 
 
-expected_salary = []
-salary = predict_salary('Программист Python')
-for vacancy in salary:
-    if vacancy['salary']['from'] and vacancy['salary']['to'] and vacancy['salary']['currency'] == 'RUR':
-        expected_salary.append((vacancy['salary']['from'] + vacancy['salary']['to']) / 2)
-        if vacancy['salary']['from'] and vacancy['salary']['to'] is None:
-            expected_salary.append(vacancy['salary']['from'] * 1.2)
-            if vacancy['salary']['to'] and vacancy['salary']['from'] is None:
-                expected_salary.append(vacancy['salary']['to'] * 0.8)
-
-print(expected_salary)
+if __name__ == '__main__':
+    print(programmer_languages)
