c8ca0b326763a404b3adca035622c13d9fcf9ccc
Yegor <kutuzov-1313@yandex.ru>
2021-03-21 16:14:01 +0300

    Update text in terminale

--- terminaltable.py
+++ terminaltable.py
@@ -1,24 +1,6 @@
 from terminaltables import AsciiTable
 
 
-# TABLE_DATA = (
-#     ('Programmer Language', 'Jobs found', 'Vacancies processed', 'Average Salary'),
-#     ('0', '2007-2009', 'The Golf Mk5 Variant was\nintroduced in 2007.', '4')
-# )
-#
-#
-#
-# def main():
-#     """Main function."""
-#     title = 'Head Hunter Moscow'
-#
-#     # AsciiTable.
-#     table_instance = AsciiTable(TABLE_DATA, title)
-#     table_instance.justify_columns[2] = 'right'
-#     print(table_instance.table)
-#     print()
-
-
 def get_table_data(data):
     table_data = []
 
@@ -30,7 +12,7 @@ def get_table_data(data):
 
 
 def create_table(table_data, title):
-    header = ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
+    header = ['Programmer Languages', 'Jobs found', 'Vacancies processed', 'Average Salary']
     table_data.insert(0, header)
     table = AsciiTable(table_data)
     table.title = title
