a181c4e08a349fb296e8b1e570d77c02fd286258
kutuzov_em <kutuzov-1313@yandex.ru>
2021-03-24 16:31:37 +0300

    Refactoring logic

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 def predict_salary(salary_from, salary_to):
-    if salary_from != 0 and salary_to != 0:
-        return int(salary_from + salary_to / 2)
-    elif salary_from == 0 and salary_to != 0:
-        return salary_to * 1.2
-    elif salary_from != 0 and salary_to == 0:
-        return salary_from * 0.8
+    avg_salary = None
+
+    if salary_from and salary_to:
+        avg_salary = (salary_from + salary_to) / 2
+    elif salary_from:
+        avg_salary = salary_from * 1.2
+    elif salary_to:
+        avg_salary = salary_to * 0.8
+
+    return avg_salary
