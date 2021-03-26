03ccc507b2b9a69409d94172f545312658b89257
kutuzov_em <kutuzov-1313@yandex.ru>
2021-03-26 18:04:23 +0300

    UPPER_CASE for global var PROGRAMMING_LANGUAGES

--- future_salary.py
+++ future_salary.py
@@ -4,24 +4,24 @@ from statistic_sj import get_statistic_sj
 
 from dotenv import load_dotenv
 
-top_programmer_languages = ['JavaScript',
-                            'Java',
-                            'Python',
-                            'Ruby',
-                            'PHP',
-                            'C++',
-                            'C#',
-                            'C',
-                            'Go',
-                            'Swift']
+PROGRAMMING_LANGUAGES = ['JavaScript',
+                         'Java',
+                         'Python',
+                         'Ruby',
+                         'PHP',
+                         'C++',
+                         'C#',
+                         'C',
+                         'Go',
+                         'Swift']
 
 
 def main():
     load_dotenv()
     print('Wait for the data to be received...')
 
-    hh_info = get_statistic_hh(top_programmer_languages)
-    sj_info = get_statistic_sj(top_programmer_languages)
+    hh_info = get_statistic_hh(PROGRAMMING_LANGUAGES)
+    sj_info = get_statistic_sj(PROGRAMMING_LANGUAGES)
 
     hh_table_data = get_table_data(hh_info)
     sj_table_data = get_table_data(sj_info)
