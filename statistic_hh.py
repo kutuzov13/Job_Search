dd4e3e4d8b8938f854f61abdcd6605fa87be8d52
kutuzov_em <kutuzov-1313@yandex.ru>
2021-07-08 18:49:35 +0300

    Refactoring

--- statistic_hh.py
+++ statistic_hh.py
@@ -1,24 +1,29 @@
+"""Get statistics of programmer vacancies from the HeadHunter website."""
+
 import statistics
 from itertools import count
-from typing import Tuple, Dict
+from typing import Dict, Tuple
 
 import requests
-
 from utils import predict_salary
+
+MOSCOW_ID = 1
 
 
 def fetch_vacancies(program_lang: str) -> Tuple:
-    moscow_id = 1
+    """Get an average salary depending on the programming language and the number of jobs."""
     head_hunter_api = 'https://api.hh.ru/vacancies'
     headers = {'User-Agent': 'HH-User-Agent'}
-    params = {'text': f'Программист {program_lang}',
-              'area': moscow_id,
-              'only_with_salary': 'true'}
+    payload = {
+        'text': f'Программист {program_lang}',
+        'area': MOSCOW_ID,
+        'only_with_salary': 'true',
+    }
 
     salaries = []
     for page in count(0):
-        params['pages'] = page
-        response = requests.get(head_hunter_api, params=params, headers=headers)
+        payload['pages'] = page
+        response = requests.get(head_hunter_api, params=payload, headers=headers)
         response.raise_for_status()
         page_data = response.json()
         vacancies_found = page_data['found']
@@ -34,12 +39,15 @@ def fetch_vacancies(program_lang: str) -> Tuple:
 
 
 def get_statistic_hh(programmer_languages: list) -> Dict:
+    """Get job statistics for a programming language."""
     job_statistics = {}
 
     for program_language in programmer_languages:
         salaries, vacancies_found = fetch_vacancies(program_language)
 
-        job_statistics[program_language] = {'vacancies_found': vacancies_found,
-                                            'vacancies_processed': len(salaries),
-                                            'average_salary': int(statistics.mean(salaries))}
+        job_statistics[program_language] = {
+            'vacancies_found': vacancies_found,
+            'vacancies_processed': len(salaries),
+            'average_salary': int(statistics.mean(salaries)),
+        }
     return job_statistics
