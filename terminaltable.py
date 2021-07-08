2777336b897e8ea3950acf2362b796d5ddd2f551
kutuzov_em <kutuzov-1313@yandex.ru>
2021-07-08 18:53:00 +0300

    Rename variable

--- terminaltable.py
+++ terminaltable.py
@@ -3,17 +3,17 @@ from typing import List
 from terminaltables import AsciiTable
 
 
-def get_table_data(data) -> List:
+def get_table_data(data_vacancies: dict) -> List:
     table_data = []
 
-    for lang, statistics in data.items():
+    for lang, statistics in data_vacancies.items():
         row = [lang, statistics['vacancies_found'], statistics['vacancies_processed'], statistics['average_salary']]
         table_data.append(row)
 
     return table_data
 
 
-def create_table(table_data, title):
+def create_table(table_data: list, title):
     header = ['Programmer Languages', 'Jobs found', 'Vacancies processed', 'Average Salary']
     table_data.insert(0, header)
     table = AsciiTable(table_data)
